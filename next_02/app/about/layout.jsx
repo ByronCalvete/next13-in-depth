60906cf5f5761f9694eea97c735e80e2d9f281cd
Byron Calvete <calvete94@gmail.com>
2023-04-29 21:08:41 -0300

    fix: change class for className

--- next_02/app/about/layout.jsx
+++ next_02/app/about/layout.jsx
@@ -2,14 +2,14 @@ import styles from './styles.module.css'
 
 export const metadata = {
   title: 'About Page',
-  description: 'Created by Byron J Calvete',
+  description: 'Created by Byron J Calvete'
 }
 
 export default function AboutLayout({ children }) {
   return (
     <>
       <nav>About Navbar</nav>
-      <main class={styles.main}>
+      <main className={styles.main}>
         {children}
       </main>
     </>
